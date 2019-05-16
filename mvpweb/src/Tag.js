c052f4461550d4afef89becc98a4b30528d4ab1a
mcb3 <matheuscberger@gmail.com>
2019-05-16 00:41:43 -0300

    error adding the data to pdf file

--- mvpweb/src/Tag.js
+++ mvpweb/src/Tag.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import QRCode from 'qrcode.react'
 import html2canvas from 'html2canvas'
+import * as jsPDF from 'jspdf'
 
 export default class Tag extends React.Component {
 
+	componentDidMount() {
+		this.savePDF();
+	}
+
 	savePDF() {
-		html2canvas(document.querySelector("#capture")).then(canvas => {
+		const input = document.querySelector("#capture");
+		html2canvas(input).then(canvas => {
 			const imgData = canvas.toDataURL('image/png');
+
+			const pdf = new jsPDF();
+			pdf.addImage(imgData, 'PNG', 0, 0);
+			pdf.save("download.pdf");
 		});
 	}
 
 	render() {
 		return (
-			<div id="capture">
+			<div id="capture" style={{padding: 10, background: "#f5da55"}}>
 				<label>OLHA A ETIQUETAA</label>
 				<br/>
 				<QRCode value="teste de novo"/>
